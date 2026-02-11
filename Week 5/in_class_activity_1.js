7b31635189da1cd94ab86b6c6cf5ae7e8638c39b
RileyLindsaySchool <rlindsay@atu.edu>
2026-02-11 16:03:30 -0600

    Added In-Class Activity 1

---
+++ Week 5/in_class_activity_1.js
@@ -0,0 +1,14 @@
+function changeColor()
+{
+    document.getElementById("change_color").style.color = "blue";
+}
+
+function printMessage()
+{
+    alert("You have put the following into the input box: " + document.getElementById("inputBox").value);
+}
+
+function consoleLog()
+{
+    console.log('You have clicked the "Console log" button');
+}
